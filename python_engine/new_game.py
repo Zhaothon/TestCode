bdad65c11812b134cac081d4876bb3493e198ee0
pythml <134953206+pythml@users.noreply.github.com>
2023-06-03 05:20:51 +0000

    q

--- python_engine/new_game.py
+++ python_engine/new_game.py
@@ -1,4 +1,16 @@
-class block:
+"""
+A New Game
+For learning
+Class
+"""
+
+
+class Block:
+    """
+    base game object:
+    Block
+    Just like a square in a map
+    """
     t_b=False
     r_b=False
     b_b=False
@@ -7,6 +19,10 @@ class block:
     def __init__(self,loca):
         self.name=loca
     def chbd(self,side):
+        """
+        Set walls
+        At the side of Block
+        """
         if side == "top":
             if self.t_b:
                 self.t_b=True
@@ -29,27 +45,53 @@ class block:
                 self.l_b=False
         else:
             warn(0)
-    def bdlist(self,bd="all"):
-        t=int(self.t_b)
-        r=int(self.r_b)
-        b=int(self.b_b)
-        l=int(self.l_b)
-        if bd == "all":
-            return [t,r,b,l]
-        elif bd == "top":
-            return [t]
-        elif bd == "right":
-            return [r]
-        elif bd == "bottom":
-            return [b]
-        elif bd == "left":
-            return [l]
+    def bdlist(self,border="all"):
+        """
+        show the walls
+        of a Block
+        """
+        top=int(self.t_b)
+        right=int(self.r_b)
+        bottom=int(self.b_b)
+        left=int(self.l_b)
+        if border == "all":
+            return [top,right,bottom,left]
+        elif border == "top":
+            return top
+        elif border == "right":
+            return right
+        elif border == "bottom":
+            return bottom
+        elif border == "left":
+            return left
         else:
             warn(1)
 
 def warn(code):
+    """
+    warn programmer to
+    use Block properly -- ToDo
+    """
     if code == 1:
         pass
+    else:
+        pass
 
-a=[]
-a.append(block("1"))
+class BlockBox:
+    """
+    base game object:
+    BlockBox
+    just like a map
+    """
+    a=[]
+    size_x=0
+    size_y=0
+    def __init__(self,x_len=5,y_len=5):
+        self.size_x=x_len
+        self.size_y=y_len
+        for i in range(x_len):
+            for j in range(y_len):
+                self.a.append(Block(str(i+1)+str(j+1)))
+
+#test
+test=BlockBox()
