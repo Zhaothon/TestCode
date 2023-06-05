f03a1d5a2197ca31796bda126c1dc5dbbb55b7f1
Zhaothon <134953206+Zhaothon@users.noreply.github.com>
2023-06-05 17:24:25 +0800

    Add files via upload

--- python_engine/new_game.py
+++ python_engine/new_game.py
@@ -14,9 +14,15 @@ class Block:
     r_b=False
     b_b=False
     l_b=False
-    nane=""
-    def __init__(self,loca):
-        self.name=loca
+    loca=[]
+    def __init__(self,loca_x,loca_y):
+        m=[loca_x,loca_y]
+        self.loca=m
+        #previous:
+        #self.loca.append(loca_x)
+        #self.loca.append(loca_y)
+        #that's the root of problem
+        del m
     def chbd(self,side):
         """
         Set walls
@@ -83,36 +89,112 @@ class BlockBox:
     BlockBox
     just like a map
     """
-    a=[]
+    block_group=[]
+    block_name=[]
     size_x=0
     size_y=0
     def __init__(self,x_len=5,y_len=5):
+        """
+        have problem:
+        why each Block in
+        BlockBox has the same
+        ID?
+        """
         self.size_x=x_len
         self.size_y=y_len
         for i in range(x_len):
             for j in range(y_len):
-                self.a.append(Block(str(i+1)+str(j+1)))
-    def choose_block(self,x_loca,y_loca):                   
-    """                                          
-    provide API
-    to operate
-    Block
-    """
-    counter=0
-    target=str(x_loca)+str(y_loca)
-    for i in self.block_name:
-        if i == target:
-            return self.block_group[counter]
-        else:
-            counter+=1
-    del i
-    return None
-    def choose_blocks(self,x_start=1,x_end=self.x_size,y_start=1,y_end=self.y_size):
-    """
-    based on
-    'choose_block':
-    choose many blocks
-    """
-    for i in range(x_end):
-        if i >= x_start:
-            pass
+                self.block_group.append(Block(i+1,j+1))
+                self.block_name.append([i+1,j+1])
+        #comparetion for debbug
+        """
+        class test:
+            lis=[]
+            ...
+            def __init__(self,*numbers):
+                ...
+                self.lis=list(numbers)#the key!
+                ...
+            ...
+
+        a=[]
+        for i in range(10):
+            for j in range(10):
+                a.append(test(i,j))
+            print(a[i].lis)
+        #successful
+        """
+        #testpoint:
+        #print(id(self.block_group[i].loca))
+        #print(self.block_group[0].loca)
+        #end test!
+        del i,j
+
+    def choose_block(self,x_loca,y_loca):
+        """
+        provide API
+        to operate
+        Block
+        """
+        counter=0
+        for i in self.block_name:
+            if i[0] == x_loca:
+                if i[1] == y_loca:
+                    return self.block_group[counter]
+                else:
+                    counter+=1
+            else:
+                counter+=1
+        return None
+    def choose_blocks(self,x_start,x_end,y_start,y_end):
+        """
+        based on 
+        'choose_block':
+        choose many blocks
+        """
+        target_x=[]
+        target_y=[]
+        targets=[]
+        returns=[]
+        for i in range(x_end):
+            if (i+1) >= x_start:
+                target_x.append(i+1)
+        #test point:
+        #print(target_x)
+        #end test!
+        del i
+        for i in range(y_end):
+            if (i+1) >= y_start:
+                target_y.append(i+1)
+        del i
+        for i in target_x:
+            for j in target_y:
+                targets.append([i,j])
+        del i,j,target_x,target_y
+        for i in targets:
+            returns.append(self.choose_block(i[0],i[1]))
+        del i,targets
+        return returns
+#testpoint-3
+"""
+test=BlockBox(1,1)
+print(test.block_group)
+"""
+#testpoint-2
+
+test=BlockBox(2,2)
+a=test.choose_blocks(int(input("x_start:")),int(input("x_end:")),int(input("y_start:")),int(input("y_end:")))
+print(a[0].loca)
+#print(i)
+
+#del i
+
+#testpoint-1
+'''
+test=BlockBox()
+for i in range(25):
+    print(test.block_name[i]+":"+str(test.block_group[i]))
+target=test.choose_block(input("x:"),input("y:"))
+print(target,target.loca)
+'''
+
