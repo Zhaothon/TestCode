d11bdf5a7013b960d7c0f0b43e87217042662226
Zhaothon <134953206+Zhaothon@users.noreply.github.com>
2023-06-12 08:33:00 +0800

    Update BlockBox.py

--- code/BlockBox.py
+++ code/BlockBox.py
@@ -16,65 +16,13 @@ class Block:
     Block
     Just like a square in a map
     """
-    t_b=False
-    r_b=False
-    b_b=False
-    l_b=False
-    loca=[]
-    def __init__(self,loca_x,loca_y):
-        m=[loca_x,loca_y]
-        self.loca=m
-        del m
-    def chbd(self,side):
-        """
-        Set walls
-        At the side of Block
-        """
-        if side == "top":
-            if self.t_b:
-                self.t_b=True
-            else:
-                self.t_b=False
-        elif side == "right":
-            if self.r_b:
-                self.r_b=True
-            else:
-                self.r_b=False
-        elif side == "bottom":
-            if self.b_b:
-                self.b_b=True
-            else:
-                self.b_b=False
-        elif side == "left":
-            if self.l_b:
-                self.l_b=True
-            else:
-                self.l_b=False
-        else:
-            warn(0)
-    def bdlist(self,border="all"):
-        """
-        show the walls
-        of a Block
-        """
-        top=int(self.t_b)
-        right=int(self.r_b)
-        bottom=int(self.b_b)
-        left=int(self.l_b)
-        if border == "all":
-            return [top,right,bottom,left]
-        elif border == "top":
-            return top
-        elif border == "right":
-            return right
-        elif border == "bottom":
-            return bottom
-        elif border == "left":
-            return left
-        else:
-            #warn(1)
-            pass
-
+    filled=False
+    loca=Loca(1,1)
+    def __init__(self,loca,fill=False):
+        self.loca=loca
+        self.filled=fill
+    def fill_status(self):
+        return self.filled
 def warn(code):
     """
     warn programmer to
